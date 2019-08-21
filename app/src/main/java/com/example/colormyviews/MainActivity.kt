d2e20969e32da51dbc60b4e74280f1d478418ac1
Kovács Ádám <the.adam.kovacs@gmail.com>
2019-08-21 12:04:02 +0200

    Added more boxes with constraints and click listeners

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -21,7 +21,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text)
+                box_four_text, box_five_text,
+                red_button, green_button, yellow_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -43,6 +44,9 @@ class MainActivity : AppCompatActivity() {
             // Boxes using Color class colors for background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             // Boxes using Android color resources for background
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
